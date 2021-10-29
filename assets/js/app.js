c9ab9a2b3c670b54d2437456feeb6ac27b109e42
xXSamXx00 <samudichio@gmail.com>
2021-10-29 12:41:32 +0200

    Struttura completata

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+const app = new Vue({
+    el: "#root",
+    data: {
+        items: ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
+        titles: ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise'],
+        text: ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,']
+    }
+});
