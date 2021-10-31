62827f076e2e5ba8be3bfaf083c941d370944733
xXSamXx00 <samudichio@gmail.com>
2021-10-31 21:00:01 +0100

    Prima parte con immagini inserite

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,24 @@ const app = new Vue({
     data: {
         items: ['img/01.jpg', 'img/02.jpg', 'img/03.jpg', 'img/04.jpg', 'img/05.jpg'],
         titles: ['Svezia', 'Svizzera', 'Gran Bretagna', 'Germania', 'Paradise'],
-        text: ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,']
+        texts: ['Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.', 'Lorem ipsum', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.', 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,', 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'],
+        activeItem: 0
+    },
+    methods: {
+        nextElement() {
+            if (this.activeItem == this.items.length - 1) {
+                this.activeItem = 0
+                console.log(activeItem);
+            } else {
+                this.activeItem = this.activeItem + 1
+            }
+        },
+        prevElement() {
+            if (this.activeItem == 0) {
+                this.activeItem = this.items.length - 1
+            } else {
+                this.activeItem = this.activeItem - 1
+            }
+        }
     }
 });
